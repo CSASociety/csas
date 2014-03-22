2859f878b1cbd407647ce42f786a3d33b345b61b
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-03-21 21:50:51 -0600

    Updated url for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Csas::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'www.richarddawkins.net' }
+  config.action_mailer.default_url_options = { :host => 'csas.herokuapp.com' }
   config.assets.precompile += %w( *.js )
 
    ActionMailer::Base.smtp_settings = {
