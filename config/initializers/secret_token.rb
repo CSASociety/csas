2b2d209b362b42b019549ac52c4dea06e07c18ea
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-03-21 21:09:48 -0600

    Initial Commit with User System

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Csas::Application.config.secret_key_base = 'a6804fea91c92853a0744087db8090448297552915e45dddf8e842a57d8737d42e3d3b50035cb243cf4b7975fe98e1b8ff6c6743fe16bd9ba7428dc09af68b30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Csas::Application.config.session_store :cookie_store, key: '_csas_session'
