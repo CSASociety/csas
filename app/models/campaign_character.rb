0e453e4cacc214c3a9268969fc060e2adab82277
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-09-05 14:58:40 -0600

    Added basics for status on campaign_characters

--- app/models/campaign_character.rb
+++ app/models/campaign_character.rb
@@ -4,6 +4,32 @@ class CampaignCharacter < ActiveRecord::Base
 
   validates :character, :presence => true, :uniqueness => {:scope => :campaign}
 
+  include AASM
+
+  aasm :column => 'status' do
+    state :adventuring, inital: true
+    state :retired
+    state :dead
+    state :missing
+
+    event :join do
+      transitions from: [:retired, :dead, :missing], to: :adventuring
+    end
+
+    event :retire do
+      transitions from: [:adventuring, :missing], to: :retired
+    end
+
+    event :kill do
+      transitions from: [:adventuring, :missing], to: :dead
+    end
+
+
+    event :lose do
+      transitions from: :adventuring, to: :missing
+    end
+
+  end
  # def name
  #   self.character.name
  # end
