2f08d4879571e0bb8b7ca75b1a044cd3ed68e488
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-09-12 12:52:35 -0600

    Fixed bug with checking user when no one is loged in on show campaigns.

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,5 +1,5 @@
 module CampaignsHelper
-  def user_is_active_player?(campaign, user)
+  def user_is_active_player?(campaign, user=nil)
     result = false
     campaign.players.active.each do |player|
       result = true if player.user_id == user.id
