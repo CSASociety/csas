808d61b661bb0cd3bccfe95d555e85dd9e5744e5
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-09-19 14:58:10 -0600

    Removed trialing slash on domain in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Csas::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'vaultofholding.com/' }
+  config.action_mailer.default_url_options = { :host => 'vaultofholding.com' }
   config.assets.precompile += %w( *.js )
 
    ActionMailer::Base.smtp_settings = {
