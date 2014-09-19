e38073a0e82e54bbb572183581b23a92e1db6bc5
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-09-18 21:48:03 -0600

    Added modals for campaign actions and moved attachment modal to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def atttachment_form(item)
+    modal_dialog :id => "attach_to",
+      :header => { :show_close => true, :dismiss => 'modal', :title => "Attach to #{item.title}" },
+      :body => render(partial: "attachments/attach", locals: {item: item, attachment: Attachment.new}),
+      :footer =>  content_tag(:button, 'Cancel', :class => 'btn btn-primary', :data => { :dismiss => 'modal' })
+  end
 end
