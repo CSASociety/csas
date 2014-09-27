94382297f864b52ec572c354625190274d424017
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-09-26 23:46:44 -0600

    added subject and recipent to mailer for events

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -7,5 +7,9 @@ class EventMailer < ActionMailer::Base
     @end = event.stop_at
     @location = event.location
     @map_link = "http://maps.google.com/?q=#{@location}"
+    if Rails.env == "staging" || Rails.env == "development"
+      subject = subject + " - #{Rails.env}"
+    end
+    mail(to: event.campaign.gm.email, subject: subject)
   end
 end
