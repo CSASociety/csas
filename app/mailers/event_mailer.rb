bf9e110f55e207686e1d426ffbfd11a31e50a593
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-10-14 15:44:40 -0600

    updated to when users are avaliable

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -6,12 +6,18 @@ class EventMailer < ActionMailer::Base
     @start = event.start_at
     @end = event.stop_at
     @location = event.location
+    user_emails = []
+    @event.campaigns.each do |campaign|
+      campaign.players.each do |player|
+        user_emails << player.user.email if player.user.email.present?
+      end
+    end
     subject = "Event starting on #{@start}"
     @map_link = "http://maps.google.com/?q=#{@location.tr(' ', '%20')}"
     if Rails.env == "staging" || Rails.env == "development"
       subject = subject + " - #{Rails.env}"
     end
-    if mail(to: "nejohannsen@gmail.com", subject: subject)
+    if mail(to: (@user_emails.empty? ? @user_emails : 'nejohannsen@gmail.com'), subject: subject)
       @event.reminder_sent = true
       @event.save
     end
