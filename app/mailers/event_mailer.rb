3ca1b563f765025d16535e6750699a38684d3b69
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-10-14 21:52:57 -0600

    Fixed logic issue in user collection for reciepiants

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -7,19 +7,19 @@ class EventMailer < ActionMailer::Base
     @end = event.stop_at
     @location = event.location
     users = []
-    user_emails = ['angelvampjk@hotmial.com', 'nicholas.johannsen@gmail.com']
-    #@event.campaigns.each do |campaign|
-    #  campaign.players.each do |player|
-    #    (users << player.user) if player.user.present?
-    #    (user_emails << player.user.email) if player.user.email.present?
-    #  end
-    #end
+    user_emails = []
+    @event.campaigns.each do |campaign|
+      campaign.players.each do |player|
+        (users << player.user) if player.user.present?
+        (user_emails << player.user.email) if player.user.email.present?
+      end
+    end
     subject = "Event starting on #{@start}"
     @map_link = "http://maps.google.com/?q=#{@location.tr(' ', '%20')}"
     if Rails.env == "staging" || Rails.env == "development"
       subject = subject + " - #{Rails.env}"
     end
-    if mail(to: 'nejohannsen@gmail.com', subject: subject)
+    if mail(to: (user_emails.present? ? user_emails : 'nejohannsen@gmail.com'), subject: subject)
       @event.reminder_sent = true
       @event.save
     end
