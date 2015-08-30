cc070edc47d69f1055d19f9b8a6a3c4b33cf2c9f
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2015-08-29 21:05:16 -0600

    update migration to fix a column

--- db/migrate/20150821201915_excahnge_character_and_playercharacter.rb
+++ db/migrate/20150821201915_excahnge_character_and_playercharacter.rb
@@ -3,5 +3,6 @@ class ExcahngeCharacterAndPlayercharacter < ActiveRecord::Migration
     rename_table :characters, :temp
     rename_table :player_characters, :characters
     rename_table :temp, :player_characters
+    rename_column :characters, :character_id, :player_character_id
   end
 end
