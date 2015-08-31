1d224b9c97b6164712f61d156c34e72138fbb0b7
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2015-08-30 21:33:52 -0600

    Fixed bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :invitable
 
   has_many :games
-  has_many :characters
+  has_many :player_characters
   has_many :resources
   has_many :players
   has_many :campaigns, through: :players
   has_many :status_approver, class_name: 'Player'
   has_one :profile
+
+  has_and_belongs_to_many :sent_emails
   has_many :assistants
   has_many :assisted_campaigns, through: :assistants, source: :campaign
-  has_and_belongs_to_many :sent_emails
-
 
 
   def self.confirmed
@@ -25,3 +25,4 @@ end
 #users = User.all
 #users.each do |user|
 #Profile.create(user_id: user.id) if user.profile.nil?
+#end
